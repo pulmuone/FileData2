4167a868212345cda9c438d05193e20a5a04e246
gwise <pulmuone.ji@gmail.com>
2021-01-17 01:15:20 +0900

    1

--- FileData2/MainActivity.cs
+++ FileData2/MainActivity.cs
@@ -49,15 +49,14 @@ namespace FileData2
             var btn2 = FindViewById<Button>(Resource.Id.button2);
             btn2.Click += (object sender, System.EventArgs e) =>
             {
-                //Intent albumIntent = new Intent(Intent.ActionOpenDocument);
-                Intent albumIntent = new Intent(Intent.ActionGetContent);
-
-                //albumIntent.AddCategory(Intent.CategoryOpenable);
+                Intent albumIntent = new Intent(Intent.ActionOpenDocument);
+                //Intent albumIntent = new Intent(Intent.ActionGetContent);
+                albumIntent.AddCategory(Intent.CategoryOpenable);
                 albumIntent.SetType("image/*");
                 albumIntent.PutExtra(Intent.ExtraAllowMultiple, true);
                 albumIntent.PutExtra(Intent.ExtraMimeTypes, new string[] { "image/*" });
-                //StartActivityForResult(albumIntent, 0);
-                StartActivityForResult(Intent.CreateChooser(albumIntent, "Select Picture"), 0);
+                StartActivityForResult(albumIntent, 0);
+                //StartActivityForResult(Intent.CreateChooser(albumIntent, "Select Picture"), 0);
             };
         }
 
